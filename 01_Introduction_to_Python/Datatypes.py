3058c38676b463c96a0c740b4ceb375233ff0af2
utkarsh9anand <83171168+utkarsh9anand@users.noreply.github.com>
2025-03-18 13:06:33 +0530

    Datatypes.py

--- 01_Introduction_to_Python/Datatypes.py
+++ 01_Introduction_to_Python/Datatypes.py
@@ -1,17 +1,17 @@
 print(14)
 print(3.14)
 print(False)
-print("Ashish")
+print("Utkarsh")
 
-print(14, 3.14,False,"Ashish")
+print(14, 3.14,False,"Utkarsh")
 
-print("Ashish"     "Jangra" "25" "3.14" "False")
-print("Ashish is a data science MENTOR")
+print("Utkarsh"     "Anand" "25" "3.14" "False")
+print("Utkarsh is a data science learner")
 
 
-print(14, 3.14,False,"Ashish")
+print(14, 3.14,False,"Utkarsh")
 
-print(14 * False * "Ashish")
+print(14 * False * "Utkarsh")
 
 print('-----------')
 
@@ -27,12 +27,12 @@ print(26 % 5)
 print('-----------')
 
 print(3.14*True)
-print('Ashish'*True)
-print('Ashish'*5)
+print('Utkarsh'*True)
+print('Utkarsh'*5)
 
-print('Ashish'*(True*5))
+print('Utkarsh'*(True*5))
 
-print('Ashish'*2)
+print('Utkarsh'*2)
 
 print('-'*10)
 
